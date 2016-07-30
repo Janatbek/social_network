f63eb8803c9fa666bb39c1f2c54c120bb60d7bb0
Janatbek <janatbek@gmail.com>
2016-07-30 11:21:40 -0700

    started aws deploying

---
+++ application/views/quotsy.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Quotsy</title>
+</head>
+<body>
+	<h1>Quotsy</h1>
+	<button>Ger Quotes</button>
+	<h3>Pablo Picasso</h3>
+	<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit."</p>
+	<h3>Albert Einstein</h3>
+	<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit."</p>
+	<h3>Malcolm X</h3>
+	<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit."</p>
+
+</body>
+</html>
